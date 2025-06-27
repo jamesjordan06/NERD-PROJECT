d9b5618b45cd8ca5b181d171d3fe12e983dfcabc
jamesjordan06 <33379490+jamesjordan06@users.noreply.github.com>
2025-06-27 02:02:16 -0400

    fix: remove explicit Promise return from dynamic page

--- app/insights/[slug]/page.tsx
+++ app/insights/[slug]/page.tsx
@@ -28,7 +28,7 @@ export default async function Page({
   params,
 }: {
   params: { slug: string };
-}): Promise<JSX.Element> {
+}) {
   const post = await fetchPostBySlug(params.slug);
 
   if (!post) {
